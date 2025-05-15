d7c0dec31d8603ae482a186fdf61d7a1500fab38
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-05-15 20:18:46 +0100

    Started on SELIC/VNA example.

---
+++ example/SELIC/example.cpp
@@ -0,0 +1,76 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "parser.h"
+
+#include <resets.h>
+#include <index.h>
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace std::chrono;
+
+using namespace boost::multiprecision;
+
+using namespace reset;
+
+
+
+inline auto parse_csv_resets_SELIC() -> resets
+{
+	// from https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados?formato=csv&dataInicial=01/07/2000&dataFinal=dd/mm/yyyy
+	return parse_csv_resets(
+		"..\\..\\..\\example\\SELIC\\data\\SELIC Effective.csv",
+		2000y / July / 1d,
+		2014y / July / 1d
+	); // or set a working directory?
+}
+
+
+
+int main()
+{
+	// from https://wilsonfreitas.github.io/posts/pricing-brazilian-government-bonds-lft.html
+
+	const auto SELIC = parse_csv_resets_SELIC();
+
+	auto detail = index_detail{};
+	detail.initial_value = cpp_dec_float_50{ "1000" };
+	detail.initial_date = 2000y / July / 1d;
+
+	const auto date = 2014y / March / 21d;
+
+	cout
+		<< fixed
+		<< setprecision(8)
+		<< "For "
+		<< date
+		<< " VNA is "
+		<< cpp_dec_float_50{ "6023.149269" }
+		<< " and the same computed value is "
+		<< index(SELIC, date, detail);
+
+	return 0;
+}
