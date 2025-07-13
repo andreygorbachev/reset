5814916a654319eb5dda17dc999b4238b8af7e98
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-07-13 09:22:59 +0100

    Fix typo.

--- example/SONIA/example.cpp
+++ example/SONIA/example.cpp
@@ -68,7 +68,7 @@ int main()
 
 	const auto SONIA_compounded_index = parse_csv_resets_SONIA_compounded_index();
 	// I think BoE website does not fully describe the compounded index
-	// more clarify would be welcome there on how rounding is done daily (*)
+	// more clarity would be welcome there on how rounding is done daily (*)
 
 	auto detail = index_detail{};
 	detail.initial_value = cpp_dec_float_50{ 100 };
