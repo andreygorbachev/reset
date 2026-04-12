e4a67679978c7f3b51e075e8bc00acac0606c651
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-04-12 09:19:20 +0100

    Add 180 day average to the example.

--- example/SOFR/example.cpp
+++ example/SOFR/example.cpp
@@ -130,6 +130,10 @@ int main()
 	_90dd.term = days{ 90 };
 	_90dd.final_round = 5u + 2u; // as we deal with fractions, rather than rates
 
+	auto _180dd = average_detail{};
+	_180dd.term = days{ 180 };
+	_180dd.final_round = 5u + 2u; // as we deal with fractions, rather than rates
+
 	//	const auto date = 2026y / April / 10d;
 	const auto date = 2026y / April / 9d;
 
@@ -165,6 +169,17 @@ int main()
 		<< SOFR_90_day_average[date] * 100 // need a different accessor? (or handle 100 in some other way)
 		<< " and the same computed value is "
 		<< average(SOFR, date, _90dd) * 100
+		<< endl;
+
+	cout
+		<< fixed
+		<< setprecision(5)
+		<< "For "
+		<< date
+		<< " SOFR 180 Day Average is "
+		<< SOFR_180_day_average[date] * 100 // need a different accessor? (or handle 100 in some other way)
+		<< " and the same computed value is "
+		<< average(SOFR, date, _180dd) * 100
 		<< endl;
 
 	const auto& SIFMA_calendar = locate_calendar("America/SIFMA", date);
