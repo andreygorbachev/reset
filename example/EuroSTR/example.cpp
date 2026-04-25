6b9735ae661df9297205d6bf38af79ebf9e70d73
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-04-25 17:42:31 +0100

    Consider EuroSTR.

---
+++ example/EuroSTR/example.cpp
@@ -0,0 +1,59 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "parser.h"
+
+#include <decimal.h>
+#include <fixings.h>
+
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+using namespace fin_calendar;
+
+using namespace reset;
+
+
+
+static auto parse_csv_fixings_EuroSTR() -> RateFixings
+{
+	// from https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html
+	return parse_csv_fixings(
+		"Euro short-term rate.csv",
+		2019y / October / 1d,
+		2026y / April / 23d
+	);
+}
+
+
+// we ignore Pre-EuroSTR for now
+
+
+
+int main()
+{
+	const auto FTIIE = parse_csv_fixings_EuroSTR();
+
+	return 0;
+}
---
+++ example/EuroSTR/parser.h
@@ -0,0 +1,136 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <scaled_value.h>
+#include <fixings.h>
+
+#include <period.h>
+#include <weekend.h>
+#include <schedule.h>
+#include <calendar.h>
+
+#include <string>
+#include <chrono>
+#include <istream>
+#include <fstream>
+#include <stdexcept>
+#include <utility>
+#include <algorithm>
+#include <cassert>
+
+
+inline auto _parse_date(std::istream& fs)
+{
+	auto ymd = std::chrono::year_month_day{};
+
+#ifdef _MSC_BUILD 
+	std::chrono::from_stream(fs, "\"%Y-%m-%d\"", ymd);
+#else
+	throw std::domain_error{ "Not implemented" };
+#endif
+
+	return ymd;
+}
+
+inline auto _parse_observation(std::istream& fs)
+{
+	using namespace std::string_literals;
+
+	auto o = std::string{};
+	std::getline(fs, o);
+
+	return reset::RateFixings::observation{
+		o.substr(1uz, o.length() - 2uz)
+	}; // we ignore the first and last characters (quotes)
+}
+
+
+inline auto _parse_csv_fixings_storage(
+	std::istream& fs,
+	const std::chrono::year_month_day& from, // these could also be read from the file
+	const std::chrono::year_month_day& until
+)
+{
+	auto result = reset::RateFixings::storage{ gregorian::util::days_period{ from, until } };
+
+	for (;;)
+	{
+		const auto ymd = _parse_date(fs);
+
+		auto s = std::string{};
+		std::getline(fs, s, ','); // skip the delimiter
+		std::getline(fs, s, ','); // skip "Time Period"
+
+		const auto observation = _parse_observation(fs);
+
+		result[ymd] = observation;
+
+		if (fs.eof())
+			break;
+	}
+
+	return result;
+}
+
+
+inline auto _make_calendar(const reset::RateFixings::storage& ts)
+{
+	const auto& fu = ts.get_period();
+
+	auto hols = gregorian::schedule::dates{};
+	for (
+		auto d = fu.get_from();
+		d <= fu.get_until();
+		d = std::chrono::sys_days{ d } + std::chrono::days{ 1 }
+	)
+		if (ts[d] == std::nullopt)
+			hols.insert(d);
+
+	return gregorian::calendar{
+		gregorian::NoWeekend,
+		gregorian::schedule{ fu, hols }
+	};
+}
+
+
+inline auto parse_csv_fixings(
+	const std::string& fileName,
+	const std::chrono::year_month_day& from, // these could also be read from the file
+	const std::chrono::year_month_day& until
+) -> reset::RateFixings
+{
+	/*const*/ auto fs = std::ifstream{ fileName }; // should we handle a default .csv file extension?
+	assert(fs);
+
+	// skip the header
+	auto t = std::string{};
+	std::getline(fs, t);
+
+	auto ts = _parse_csv_fixings_storage(fs, from, until);
+	// we can check the fixings vs decimal places
+
+	auto c = _make_calendar(ts);
+
+	return reset::fixings{ std::move(ts), std::move(c), 3u };
+}
