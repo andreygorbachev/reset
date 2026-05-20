741fbc3390bff9736297e86a86b90da0829fcee3
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-20 19:02:46 +0100

    Started on TONA.

---
+++ example/TONA/example.cpp
@@ -0,0 +1,77 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "parser.h"
+
+#include <decimal.h>
+#include <scaled_value.h>
+#include <fixings.h>
+#include <index.h>
+#include <average.h>
+
+#include <actual_360.h>
+
+#include <static_data.h>
+#include <calendar.h>
+#include <schedule.h>
+#include <period.h>
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+
+using namespace std;
+using namespace std::chrono;
+
+using namespace gregorian;
+using namespace gregorian::util;
+using namespace gregorian::static_data;
+
+using namespace fin_calendar;
+
+using namespace reset;
+
+
+
+static auto parse_csv_fixings_TONA() -> RateFixings
+{
+	// from https://www.stat-search.boj.or.jp/index_en.html
+	return parse_csv_fixings<RateFixings>(
+		"FM01.csv",
+		1998y / January / 5d,
+		2026y / May / 18d,
+		3u
+	);
+}
+
+
+
+int main()
+{
+	const auto TONA = parse_csv_fixings_TONA();
+
+	return 0;
+}
