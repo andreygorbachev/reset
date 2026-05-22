111bd9b08aa5da58548aa3a4b6df349c459035e7
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-22 15:53:05 +0100

    Consider factoring out the example parser into a separate little library.

--- example/SARON/example.cpp
+++ example/SARON/example.cpp
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "parser.h"
+#include <parser.h>
 
 #include <decimal.h>
 #include <scaled_value.h>
