d0f6ab8caa1ab96a4450e82637cf7a1dabb48ad4
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-23 23:56:02 +0100

    Started on PolSTR.

---
+++ example/PolSTR/example.cpp
@@ -0,0 +1,296 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <parser.h>
+
+#include <decimal.h>
+#include <scaled_value.h>
+#include <fixings.h>
+#include <index.h>
+#include <average.h>
+
+#include <modified_preceding.h>
+#include <actual_365_fixed.h>
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <cassert>
+#include <optional>
+
+using namespace std;
+using namespace std::chrono;
+
+using namespace fin_calendar;
+
+using namespace reset;
+
+/*If a potential Reference Period start date, i.e.resulting from the calendar
+shift5 of a given Reference Period end date backwards by a certain number
+of months, would fall on a day that is not a Business Day or a non - existent
+day(e.g. 30th February), then the nearest Business Day preceding this
+potential Reference Period start date is selected as the Reference Period
+start date, unless it falls already in the month preceding the month of the
+potential Reference Period start date.In this case, the nearest Business Day
+following the potential Reference Period start date shall be selected as the
+Reference Period start date.If, in turn, the potential Reference Period start
+date is a Business Day, that Business Day is selected as the Reference
+Period start date*/
+
+// from https://gpwbenchmark.pl/delayed_data
+// and https://gpwbenchmark.pl/historical_data
+
+static auto make_parser_detail() -> parser_detail
+{
+	return parser_detail{
+		1u,
+		2021y / January / 4d,
+		2026y / May / 14d,
+		"%Y-%m-%d",
+		',',
+		nullopt,
+		nullopt,
+		0u
+	};
+}
+
+static auto parse_csv_fixings_PolSTR() -> RateFixings
+{
+	const auto d = make_parser_detail();
+
+	return parse_csv_fixings<RateFixings>(
+		".csv",
+		d,
+		"Europe/Warsaw",
+		3u
+	);
+}
+/*
+static auto parse_csv_fixings_PolSTR_compounded_index() -> IndexFixings
+{
+	return parse_csv_fixings<IndexFixings>(
+		"Compounded euro-short term rates and index.csv",
+		1u,
+		2019y / October / 1d,
+		2026y / April / 24d,
+		8u
+	);
+}
+
+static auto parse_csv_fixings_PolSTR_1_month_compounded() -> RateFixings
+{
+	return parse_csv_fixings<RateFixings>(
+		"Compounded euro-short term rates and index.csv",
+		3u,
+		2019y / November / 1d,
+		2026y / April / 24d,
+		5u
+	);
+}
+
+static auto parse_csv_fixings_PolSTR_3_month_compounded() -> RateFixings
+{
+	return parse_csv_fixings<RateFixings>(
+		"Compounded euro-short term rates and index.csv",
+		4u,
+		2020y / January / 2d,
+		2026y / April / 24d,
+		5u
+	);
+}
+
+static auto parse_csv_fixings_PolSTR_6_month_compounded() -> RateFixings
+{
+	return parse_csv_fixings<RateFixings>(
+		"Compounded euro-short term rates and index.csv",
+		5u,
+		2020y / April / 1d,
+		2026y / April / 24d,
+		5u
+	);
+}
+*/
+
+
+int main()
+{
+	const auto PolSTR = parse_csv_fixings_PolSTR();
+/*
+	auto rfd = rate_fixing_detail{};
+	rfd.day_count = actual_365_fixed<Decimal>{};
+
+	const auto PolSTR_compounded_index = parse_csv_fixings_PolSTR_compounded_index();
+
+	const auto PolSTR_1_month_compounded = parse_csv_fixings_PolSTR_1_month_compounded();
+	const auto PolSTR_3_month_compounded = parse_csv_fixings_PolSTR_3_month_compounded();
+	const auto PolSTR_6_month_compounded = parse_csv_fixings_PolSTR_6_month_compounded();
+
+	// from https://gpwbenchmark.pl/documentation-transaction-based
+	auto id = index_detail{};
+	id.initial_value = Value{ "100" };
+	id.initial_date = 2019y / October / 1d;
+	id.final_round = 8u;
+
+	auto _1md = average_detail{};
+	_1md.term = months{ 1 };
+	_1md.business_day_convention = modified_preceding{};
+	_1md.final_round = 5u + 2u; // as we deal with fractions, rather than rates
+
+	auto _3md = average_detail{};
+	_3md.term = months{ 3 };
+	_3md.business_day_convention = modified_preceding{};
+	_3md.final_round = 5u + 2u; // as we deal with fractions, rather than rates
+
+	auto _6md = average_detail{};
+	_6md.term = months{ 6 };
+	_6md.business_day_convention = modified_preceding{};
+	_6md.final_round = 5u + 2u; // as we deal with fractions, rather than rates
+
+	const auto date = 2026y / April / 24d;
+
+	const auto& indx = PolSTR_compounded_index[date];
+	assert(indx);
+
+	cout
+		<< fixed
+		<< setprecision(PolSTR_compounded_index.get_decimal_places())
+		<< "For "
+		<< date
+		<< " PolSTR Compounded Index is "
+		<< indx->get_value()
+		<< " and the same computed value is "
+		<< index(PolSTR, rfd, date, id).get_value()
+		<< endl;
+
+	const auto& _1m_cmp = PolSTR_1_month_compounded[date];
+	assert(_1m_cmp);
+
+	cout
+		<< fixed
+		<< setprecision(PolSTR_1_month_compounded.get_decimal_places())
+		<< "For "
+		<< date
+		<< " PolSTR 1 Month Compounded Average is "
+		<< _1m_cmp->get_value()
+		<< " and the same computed value is "
+		<< average(PolSTR, rfd, date, _1md).percent.get_value()
+		<< endl;
+
+	const auto& _3m_cmp = PolSTR_3_month_compounded[date];
+	assert(_3m_cmp);
+
+	cout
+		<< fixed
+		<< setprecision(PolSTR_3_month_compounded.get_decimal_places())
+		<< "For "
+		<< date
+		<< " PolSTR 3 Month Compounded Average is "
+		<< _3m_cmp->get_value()
+		<< " and the same computed value is "
+		<< average(PolSTR, rfd, date, _3md).percent.get_value()
+		<< endl;
+
+	const auto& _6m_cmp = PolSTR_6_month_compounded[date];
+	assert(_6m_cmp);
+
+	cout
+		<< fixed
+		<< setprecision(PolSTR_6_month_compounded.get_decimal_places())
+		<< "For "
+		<< date
+		<< " PolSTR 6 Month Compounded Average is "
+		<< _6m_cmp->get_value()
+		<< " and the same computed value is "
+		<< average(PolSTR, rfd, date, _6md).percent.get_value()
+		<< endl;
+
+	// look for inconsistencies in the data
+
+	const auto& PolSTR_1_month_compounded_calendar = PolSTR_1_month_compounded.get_calendar();
+	const auto _1_month_dates = PolSTR_1_month_compounded_calendar.make_business_days_schedule(
+		PolSTR_1_month_compounded.get_time_series().get_period()
+	);
+	for (const auto& d : _1_month_dates.get_dates())
+	{
+		const auto& _1m_avg = PolSTR_1_month_compounded[d];
+		assert(_1m_avg);
+
+		if (*_1m_avg != average(PolSTR, rfd, d, _1md).percent)
+			cout
+			<< fixed
+			<< setprecision(PolSTR_1_month_compounded.get_decimal_places())
+			<< "For "
+			<< d
+			<< " PolSTR 1 Month Compounded Average is "
+			<< PolSTR_1_month_compounded[d]->get_value()
+			<< " and the same computed value is "
+			<< average(PolSTR, rfd, d, _1md).percent.get_value()
+			<< endl;
+	}
+
+	const auto& PolSTR_3_month_compounded_calendar = PolSTR_3_month_compounded.get_calendar();
+	const auto _3_month_dates = PolSTR_3_month_compounded_calendar.make_business_days_schedule(
+		PolSTR_3_month_compounded.get_time_series().get_period()
+	);
+	for (const auto& d : _3_month_dates.get_dates())
+	{
+		const auto& _3m_avg = PolSTR_3_month_compounded[d];
+		assert(_3m_avg);
+
+		if (*_3m_avg != average(PolSTR, rfd, d, _3md).percent)
+			cout
+			<< fixed
+			<< setprecision(PolSTR_3_month_compounded.get_decimal_places())
+			<< "For "
+			<< d
+			<< " PolSTR 3 Month Compounded Average is "
+			<< PolSTR_3_month_compounded[d]->get_value()
+			<< " and the same computed value is "
+			<< average(PolSTR, rfd, d, _3md).percent.get_value()
+			<< endl;
+	}
+
+	const auto& PolSTR_6_month_compounded_calendar = PolSTR_6_month_compounded.get_calendar();
+	const auto _6_month_dates = PolSTR_6_month_compounded_calendar.make_business_days_schedule(
+		PolSTR_6_month_compounded.get_time_series().get_period()
+	);
+	for (const auto& d : _6_month_dates.get_dates())
+	{
+		const auto& _6m_avg = PolSTR_6_month_compounded[d];
+		assert(_6m_avg);
+
+		if (*_6m_avg != average(PolSTR, rfd, d, _6md).percent)
+			cout
+			<< fixed
+			<< setprecision(PolSTR_6_month_compounded.get_decimal_places())
+			<< "For "
+			<< d
+			<< " PolSTR 6 Month Compounded Average is "
+			<< PolSTR_6_month_compounded[d]->get_value()
+			<< " and the same computed value is "
+			<< average(PolSTR, rfd, d, _6md).percent.get_value()
+			<< endl;
+	}
+*/
+	return 0;
+}
